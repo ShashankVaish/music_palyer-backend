8782deef70a9999bf0341715a96bc818a4ec0897
Your Name <you@example.com>
2025-04-06 12:17:16 +0530

    add multer file

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,13 @@
+import multer from "multer"
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public/tmp')
+    },
+    filename: function (req, file, cb) {
+    
+      cb(null, file.filename)
+    }
+  })
+  
+  const upload = multer({ storage: storage })
+  export {upload}
